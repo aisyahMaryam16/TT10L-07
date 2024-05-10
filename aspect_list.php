3d632e4cf042216c742344fe5cf3f84df08e0edd
aisyah maryam <maryam161105@gmail.com>
2024-05-11 01:31:03 +0800

    added the coding to connect the table list to the database in phpmyadmin including the table closing tag

--- aspect_list.php
+++ aspect_list.php
@@ -15,3 +15,16 @@
         <th>Total Marks</th>
         <th colspan="2">Action</th>
     </tr>
+    <?php
+        $sql = "select * from aspect";
+        $result = mysqli_query($connect, $sql);
+        while($value = mysqli_fetch_array($result)) {
+            echo "<tr><td>$value[AspectID]</td>
+                <td>$value[aspect]</td>
+                <td>$value[TotalMarks]</td>
+                <td><a href='aspect_update.php?AspectID=$value[AspectID]'>update</a></td>
+                <td><a href='aspect_delete.php?AspectID=$value[AspectID]'>delete</a></td>
+                </tr>";
+        }
+    ?>
+</table>
