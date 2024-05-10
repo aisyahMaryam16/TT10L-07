e1f8ed8a615e57238e6f8e572cc0193222e354c8
aisyah maryam <maryam161105@gmail.com>
2024-05-11 01:22:17 +0800

    typed the coding for the list of aspects

--- aspect_list.php
+++ aspect_list.php
@@ -1,28 +1,17 @@
 <?php 
-    include ('connect.php'); 
-    include("supervisor_menu.php"); 
-?> 
- 
-<link rel="stylesheet" href="list.css"> 
- 
-<table> 
-    <caption>LIST CRITERIA</caption> 
-    <tr> 
-        <th>ID</th> 
-        <th>Aspect</th> 
-        <th>Total Marks</th> 
-        <th colspan="2">Action</th> 
-    </tr> 
-    <?php 
-        $sql = "select * from Aspect"; 
-        $result = mysqli_query($connect, $sql); 
-        while($value = mysqli_fetch_array($result)) { 
-            echo "<tr><td>$value[AspectID]</td> 
-                <td>$value[aspect]</td> 
-                <td>$value[TotalMarks]</td> 
-                <td><a href='aspect_update.php?AspectID=$value[AspectID]'>update</a></td> 
-                <td><a href='aspect_delete.php?AspectID=$value[AspectID]'>delete</a></td> 
-        </tr>"; 
-        } 
-    ?> 
-    </table>
+    include('connect.php');
+    include("supervisor_menu.php");
+?>
+
+
+<link rel="stylesheet" href="list.css">
+
+
+<table>
+    <caption>ASPECT LIST</caption>
+    <tr>
+        <th>ID</th>
+        <th>Aspect</th>
+        <th>Total Marks</th>
+        <th colspan="2">Action</th>
+    </tr>
