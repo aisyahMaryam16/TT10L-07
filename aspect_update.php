dbf39db9c6e6561c28b0c45de11fbb79cfe80238
aisyah maryam <maryam161105@gmail.com>
2024-05-11 01:39:58 +0800

    edited some lines of the coding that has a typo

--- aspect_update.php
+++ aspect_update.php
@@ -6,34 +6,34 @@
         $AspectID = $_POST["AspectID"]; 
         $aspect = $_POST["aspect"]; 
         $TotalMarks = $_POST["TotalMarks"]; 
-         
-        $sql = "update Aspect set aspect = '$aspect', TotalMarks = '$TotalMarks' 
-                where AspectID = '$AspectID' 
-"; 
+        $sql = "update aspect set aspect = '$aspect', TotalMarks = '$TotalMarks' 
+                where AspectID = '$AspectID' "; 
                  
-                $result = mysqli_query($xonnect, $sql); 
-                if ($result == true) 
-                    echo "<script>alert('Successfully update'); 
-                    window.location='aspect_list.php'</script>"; 
-else 
-echo "<br><center>Error : $sql<br>".mysqli_error($connect)."</center>"; 
+        $result = mysqli_query($connect, $sql); 
+        if ($result == true) 
+        echo "<script>alert('successfully updated'); 
+             window.location='aspect_list.php'</script>"; 
+    else 
+        echo "<br><center>Error : $sql<br>".mysqli_error($connect)."</center>"; 
     } 
      
-    if (isset($_GET['AspectID'])) 
-    $AspectID = $_GET['AspectID']; 
+        if (isset($_GET['AspectID'])) 
+        $AspectID = $_GET['AspectID']; 
      
-$sql = "select * from Aspect where AspectID = '$AspectID' "; 
-$result = mysqli_query($connect, $sql); 
-while($value = mysqli_fetch_array($result)) { 
-    $aspect = $value['aspect']; 
-    $TotalMarks = $value['TotalMarks']; 
-}  
+        $sql = "select * from aspect where AspectID = '$AspectID' "; 
+        $result = mysqli_query($connect, $sql);
+
+    while($value = mysqli_fetch_array($result)) { 
+        
+        $aspect = $value['aspect']; 
+        $TotalMarks = $value['TotalMarks']; 
+    }  
 ?> 
  
 <link rel="stylesheet" href="form.css"> 
 <link rel="stylesheet" href="button.css"> 
  
-<h3 class="long">CRITERIA UPDATE</h3> 
+<h3 class="long">ASPECT UPDATE</h3> 
 <form class="long" action="aspect_update.php" method="post"> 
 <table> 
     <tr> 
@@ -45,9 +45,9 @@ while($value = mysqli_fetch_array($result)) {
         <td><input type="text" name="aspect" value= "<?php echo $aspect; ?>"></td> 
     </tr> 
     <tr> 
-        <td>TotalMarks</td> 
+        <td>Total Marks</td> 
         <td><input type="text" name="TotalMarks" value= "<?php echo $TotalMarks; ?>"></td> 
     </tr> 
     </table> 
-<button class="update" type="submit" name="submit">Update</button> 
+<button class="update" type="submit" name="submit">update</button> 
 </form>
