8b5d171ad8d6d90fb42bebb25d7760191bc5a20a
tulip <tulipazalea887@gmail.com>
2024-05-13 21:37:10 +0800

    added steward_menu

---
+++ steward_menu.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="menu.css">
+<body>
+<div class="menu">
+    <div class="judge">
+        <h2>ROBOTICS COMPETITION MANAGEMENT SYSTEM</h2>
+        <ul>
+            <li>
+<a href="judge_home.php">
+<b>HOME</b></a>
+</li>
+        </ul>
+        <ul>
+            <li>
+<a href="#">
+<b>FORM</b></a>
+                <ul>
+                    <li><a href="judge_form.php">Show</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>
+<a href="#">
+<b>LIST</b></a>
+                <ul>
+                    <li><a href="judge_contestant.php">Show</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>
+<a href="logout.php">
+<b>EXIT</b></a>
+</li>
+        </ul>
+    </div>
+</div>
+</body>
+        
