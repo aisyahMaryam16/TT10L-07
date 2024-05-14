d2c0da018e6485aa702c1d471cd2979fcc20cb84
aisyah maryam <maryam161105@gmail.com>
2024-05-14 14:56:11 +0800

    updated coding

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $password = '';
-    $database = 'rcms';
+    $database = 'rocs';
     
     $sambungan = mysqli_connect($host, $user,
     $password, $database)
