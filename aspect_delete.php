d851c547edc6d3d0e74a750658c80b17abdf3588
aisyah maryam <maryam161105@gmail.com>
2024-06-04 21:03:45 +0800

    corrected a typo in the file

--- aspect_delete.php
+++ aspect_delete.php
@@ -12,7 +12,7 @@
                 echo "<script>alert('successfully deleted');
                 window.location='aspect_list.php</script>";
             else
-                echo "<script>alert('unable to delete. No record was found.');
+                echo "<script>alert('Unable to delete. No record was found.');
                 window.location='aspect_list.php'</script>";
         }
     else
@@ -27,12 +27,12 @@
 <link rel="stylesheet" href="button.css">
    
 <h3 class="long">DELETE ASPECT</h3>
-   <form class="long" action="aspect_delete.php" method="post">
-       <table>
-        <tr>
-            <td>Aspect</td>
-            <td><input type="text" name="AspectID" value = "<?php echo $AspectID; ?>" ></td>
-        </tr>
-       </table>
-       <button class="delete" type="submit" name="submit">Delete</button>
-   </form>
+<form class="long" action="aspect_delete.php" method="post">
+    <table>
+    <tr>
+        <td>Aspect ID</td>
+        <td><input type="text" name="AspectID" value = "<?php echo $AspectID; ?>" ></td>
+    </tr>
+    </table>
+    <button class="delete" type="submit" name="submit">Delete</button>
+</form>
