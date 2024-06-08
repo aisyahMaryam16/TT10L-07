ceaa3aa4026e7ce2f0724ba85d44706f5e941eac
aisyah maryam <maryam161105@gmail.com>
2024-06-08 22:26:58 +0800

    modifies some lines to run it perfectly

--- player_search.php
+++ player_search.php
@@ -19,7 +19,7 @@
                     $data = mysqli_query($connect, $sql); 
                     while ($player = mysqli_fetch_array($data)) { 
                         echo "<option value='$player[IcNumber]'>$player[IcNumber] : 
-                            $player[PlayerName]</option>"; 
+                            $player[player]</option>"; 
                     } // tamat while 
                 ?> 
             </select> 
