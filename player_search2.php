e20d048c2a3e928c916bb4fe01a697e41fb5d829
aisyah maryam <maryam161105@gmail.com>
2024-06-08 22:27:14 +0800

    modifies some lines to run it perfectly

--- player_search2.php
+++ player_search2.php
@@ -2,16 +2,27 @@
     include('connect.php'); 
     include("supervisor_menu.php"); 
      
-    $IcNumber = $_POST['IcNumber']; 
-     
-    $sql = "select * from player where IcNumber = '$IcNumber'"; 
-    $result = mysqli_query($connect, $sql); 
-    $player = mysqli_fetch_array($result); 
-     
-    $PlayerName = $player['PlayerName']; 
-    $StewardID = $player['StewardID']; 
-    $SupervisorID = $player['StewardID']; 
-    $Password = $player['Password']; 
+    if(isset($_POST['IcNumber'])){ 
+        $IcNumber = $_POST['IcNumber']; 
+         
+        $sql = "select * from player where IcNumber = '$IcNumber'"; 
+        $result = mysqli_query($connect, $sql); 
+         
+        if(mysqli_num_rows($result) > 0){ 
+            $player = mysqli_fetch_array($result); 
+             
+            $playerName = $player['player']; 
+            $StewardID = $player['StewardID']; 
+            $SupervisorID = $player['SupervisorID']; 
+            $password = $player['password']; 
+        } else { 
+            echo "No player found with IC Number $IcNumber"; 
+            exit; 
+        } 
+    } else { 
+        echo "IC Number is required"; 
+        exit; 
+    } 
 ?> 
  
 <link rel="stylesheet" href="list.css"> 
@@ -27,7 +38,7 @@
     </tr> 
     <tr> 
         <td class="result">Player Name</td> 
-        <td class="result"><?php echo $PlayerName; ?></td> 
+        <td class="result"><?php echo $playerName; ?></td> 
     </tr> 
     <tr> 
         <td class="result">Steward ID</td> 
@@ -39,6 +50,6 @@
     </tr> 
     <tr> 
         <td class="result">Password</td> 
-        <td class="result"><?php echo $Password; ?></td> 
+        <td class="result"><?php echo $password; ?></td> 
     </tr> 
 </table>
