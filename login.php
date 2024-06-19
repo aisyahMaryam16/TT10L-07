58fbe47ba4cc5c0b57356b57a3ddbbb039168a6d
aisyah <maryam161105@gmail.com>
2024-06-19 23:47:17 +0800

    edited the html

--- login.php
+++ login.php
@@ -65,32 +65,29 @@ ob_start();
 ?>
 
 <html>
-    <link rel="stylesheet" href="form.css">
-    <link rel="stylesheet" href="button.css">
+   <head>
+   <title>Log In</title>
+    <link rel="stylesheet" href="styles3.css">
+    </head>
     
-    <body>
-        <center>
-            <img src=''><br>
-            <img src=''>
-        </center>
-        <h3 class="short">LOG IN</h3>
-        <form class="short" action="login.php" method="post">
-            <table>
-            <tr>
-                <td><img src="user.png"></td>
-            <td>
-        <input type="text" name="UserID" placeholder="UserID" required>
-            </td>
-            </tr>
-            <tr>
-                <td><img src="lock.png"></td>
-            <td>
-        <input type="password" name="password" placeholder="Passcode" required>
-            </td>
-            </tr>
-            </table>
-                <button class="login" type="submit" name="submit">Login</button>
-                <button class="signup" type="button" onclick="window.location='signup.php'">Sign Up</button>
-            </form>
-        </body>
-    </html>
+<body>
+        <div class="wrapper">
+        <form action="" method="post">
+    <h1>Login</h1>
+    <div class="input-box">
+        <input type="text" class="input-field" placeholder="ID" name="UserID" required>
+        <i class="bx bxs-user"></i>
+    </div>
+    <div class="input-box">
+        <input type="password" class="input-field" placeholder="password" required name="password">
+        <i class="bx bxs-user"></i>
+    </div>
+    <button type="submit" class="btn" name="submit">Login</button>
+    <div class="register-link">
+        <p>Don't Have An Account? <a href="signup.php">Register</a></p>
+    </div>
+</form>
+
+    </div>
+</body>
+</html>
