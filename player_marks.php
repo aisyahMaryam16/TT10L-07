dcdf54df44eb241104a9c3167c69040006ab5c0f
tulip <tulipazalea887@gmail.com>
2024-06-22 17:40:14 +0800

    edited line 45 & 53

--- player_marks.php
+++ player_marks.php
@@ -1,63 +1,33 @@
 <?php 
-    session_start(); 
-    include ('connect.php'); 
-    include("player_menu.php"); 
-    $name = $_SESSION["name"]; 
-    $IcNumber = $_SESSION["UserID"]; 
-     
-    $position = 0; 
-    $sql = "select * from result 
-        join player on result.IcNumber = player.IcNUmber 
-        join aspect on result.AspectID = aspect.AspectID 
-        join steward on player.StewardID = steward.StewardID 
-        group by player.IcNumber order by result.Score desc"; 
-         
-    $data = mysqli_query($connect, $sql); 
-    $No = 0; 
-    while ($result = mysqli_fetch_array($data)) { 
-        $No = $No + 1; 
-        if ($result['IcNumber'] == $IcNumber) 
-            $position = $No; 
-             
-    }// tamat while 
-?> 
+    include('connect.php'); 
+    include("supervisor_menu.php"); 
+    ?> 
  
-<link rel="stylesheet" href="list.css"> 
+    <link rel="stylesheet" href="list.css"> 
  
-<table> 
-    <caption>Player Name : <?php echo $name ?></caption> 
+    <table>  
     <tr> 
-        <th>Aspect</th> 
-        <th>Total Marks</th> 
-        <th>Score Obtained</th> 
+        <th>Ic Number</th> 
+        <th>Name</th> 
+        <th>Steward</th> 
+        <th>Supervisor</th> 
+        <th>Password</th> 
+        <th colspan="2">Action</th> 
     </tr> 
+     
     <?php 
-        $sql = "select * from result 
-            join aspect on result.AspectID = aspect.AspectID 
-            where IcNumber = '$IcNumber' "; 
-        $data = mysqli_query($connect, $sql); 
-        $NoRec = mysqli_num_rows($data); 
-        if ($NoRec > 0)  
-{ 
-            while ($result = mysqli_fetch_array($data))  
-{ 
-                echo "<tr > 
-                    <td>$result[aspect]</td> 
-                    <td>$result[TotalMarks]</td> 
-                    <td>$result[ScoreObtained]</td> 
-                    </tr>"; 
-                $total_marks = $result['Score']; 
-            } 
-            echo "<tr class='total_marks'> <td ></td> 
-                    <td class='total_marks'>Score</td> 
-                    <td>$total_marks</td> 
-                    </tr>"; 
-            if ($position != 0) 
-                echo "<tr class='marks_total'><td ></td> 
-                    <td class='marks_total'>Position</td>
-                    <td>$position/$No</td>
-                    </tr></table>";
-        else { 
-            echo "<tr ><td>marks</td><td>haven't</td><td>calculated</td> 
-                    </tr></table>"; 
-        } ?>
+        $sql = "select * from player"; 
+        $result = mysqli_query($connect, $sql); 
+        while($player = mysqli_fetch_array($result)) {
+            $maskedPassword = str_repeat('*', strlen($player['password']));
+         echo "<tr> <td>$player[IcNumber]</td> 
+                <td class='name'>$player[player]</td> 
+                <td>$player[StewardID]</td> 
+                <td>$player[SupervisorID]</td> 
+                <<td>{$maskedPassword}</td>
+                <td><a href='player_update.php?IcNumber=$player[IcNumber]'class='btn edit'>update</a></td> 
+                <td><a href='player_delete.php?IcNumber=$player[IcNumber]'class='btn btn-danger'>delete</a></td> 
+            </tr>"; 
+        } //tamat while 
+    ?> 
+</table>
