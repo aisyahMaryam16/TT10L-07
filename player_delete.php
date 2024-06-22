4ecfb298cd01f5e52d0eec609d5ce44862b6638c
tulip <tulipazalea887@gmail.com>
2024-06-22 12:03:26 +0800

    edited line 31 & 32

--- player_delete.php
+++ player_delete.php
@@ -28,8 +28,8 @@
     <link rel="stylesheet" href="form.css"> 
     <link rel="stylesheet" href="button.css"> 
      
-    <h3 class="long">DELETE PLAYER</h3> 
-    <form class="long" action="player_delete.php" method="post"> 
+    <h3 class="short">DELETE PLAYER</h3> 
+    <form class="short" action="player_delete.php" method="post"> 
         <table> 
             <tr> 
                 <td>IcNumber</td> 
