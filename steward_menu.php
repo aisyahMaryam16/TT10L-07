2be529a2b0af7e5d67caa7f36a3ba68a63689cf5
tulip <tulipazalea887@gmail.com>
2024-06-22 17:46:51 +0800

    edited line 3 - 5

--- steward_menu.php
+++ steward_menu.php
@@ -2,38 +2,33 @@
 <body>
 <div class="banner">
     <div class="navbar">
-<img src="f1logo.png" class="logo">
-<ul>
- <li>
-<a href="steward_home.php">
-<b>HOME</b></a>
-</li>
-        </ul>
-        <ul>
-            <li>
-<a href="#">
-<b>FORM</b></a>
-                <ul>
-                    <li><a href="steward_form.php">Show</a></li>
-                </ul>
-            </li>
-        </ul>
-        <ul>
-            <li>
-<a href="#">
-<b>LIST</b></a>
-                <ul>
-                    <li><a href="steward_player.php">Show</a></li>
-                </ul>
-            </li>
-        </ul>
-        <ul>
-            <li>
-<a href="logout.php">
-<b>EXIT</b></a>
-</li>
-        </ul>
+    <img src="f1logo.png" class="logo">
+    <ul>
+    <li><a href="steward_home.php">
+    <b>HOME</b></a></li>
+</ul>
+
+   <ul>
+    <li><a href="#">
+    <b>FORM</b></a>
+    <ul>
+    <li><a href="steward_form.php">Show</a></li>
+</ul>
+        </li>
+</ul>
+    <ul>
+    <li><a href="#">
+    <b>LIST</b></a>
+    <ul>
+    <li><a href="steward_player.php">Show</a></li>
+</ul>
+        </li>
+</ul>
+    <ul>
+    <li><a href="logout.php">
+    <b>EXIT</b></a></li>
+    </ul>
+        </div>
     </div>
-</div>
 </body>
         
